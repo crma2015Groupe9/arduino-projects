9e44fadfa21f086bf8f60378a3c590724e4a069d
Alexis Tessier <contact@alexistessier.com>
2015-03-19 01:23:24 +0100

    Avancement sur le SPIStackManager : pas encore fini

--- SPIStackManager/SPIStackManager.cpp
+++ SPIStackManager/SPIStackManager.cpp
@@ -2,21 +2,52 @@
 #include "SPIStackableDevice.h"
 
 SPIStackManager::SPIStackManager(SPIStackableDevice* devices, byte numberOfDevices, SPIStackableDevice_slaveSelectHandler handler){
-	_id = id;
-	_csPin = csPin;
-	_mark = mark;
+	_devices = devices;
+	_selectedDevice = 0;
+	_numberOfDevices = numberOfDevices;
+	_active = false;
 	_useHandler = true;
 	_slaveSelectHandler = handler;
 }
 
 SPIStackManager::SPIStackManager(SPIStackableDevice* devices, byte numberOfDevices){
-	
+	_devices = devices;
+	_selectedDevice = 0;
+	_numberOfDevices = numberOfDevices;
+	_active = false;
+	_useHandler = false;
 }
 
 void SPIStackManager::init(){
-	pinMode(_csPin, OUTPUT);
+	byte i;
+	for(i=0;i<_numberOfDevices;i++){
+		if(_useHandler){
+			_devices[i].isUsingCustomSelectHandler() ? _devices[i].init() : _slaveSelectHandler(HIGH, _devices[i].getID(), _devices[i].getCsPin(), _devices[i].getMark());
+		}
+		else{
+			_devices[i].init();
+		}
+	}
+}
 
-	_state = HIGH;
+void SPIStackManager::start(){
+	_active = true;
+}
 
-	_useHandler ? _slaveSelectHandler(HIGH, _id, _csPin, _mark) : digitalWrite(_csPin, HIGH);
+void SPIStackManager::select(byte deviceID){
+
+}
+
+void SPIStackManager::selectDeviceAtIndex(byte index){
+
+
+	start();
+	_devices[i];
+}
+
+void SPIStackManager::end(){
+	if(_active){
+		_devices[_selectedDevice]
+		_active = false;
+	}
 }
